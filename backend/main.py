01391717258a2ac18f3542905a823c8a0d494af5
unknown <mailcatherines@gmail.com>
2024-10-05 19:04:18 -0400

    trigger workflow

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as book_router
+import uvicorn
+import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 config = dotenv_values(".env")
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
+    app.mongodb_client = MongoClient(config.get("ATLAS_URI"))
+    app.database = app.mongodb_client[config.get("DB_NAME")]
+
 
 @app.on_event("shutdown")
 def shutdown_db_client():
@@ -19,4 +26,4 @@ def shutdown_db_client():
 def hello_world():
     return "Hello world!"
 
-app.include_router(book_router, tags=["books"], prefix="/book")
+app.include_router(book_router, prefix="/book")
