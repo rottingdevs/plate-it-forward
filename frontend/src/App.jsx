d3fb9ab1fbd4874cefd0415fff6107432ee71037
unknown <mailcatherines@gmail.com>
2024-10-04 22:32:14 -0400

    move to folders, add map

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import './App.css'
+
+function App() {
+
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3dhbnJhYzIiLCJhIjoiY2x4Zzd2aWs3MHpyejJrcG9zdjVuN3Y0ayJ9.NpYBe00KtZ6Q00KmLqgm3A'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-79.4, 43.6],
+      zoom: 9
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
+  return (
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
+  )
+}
+
+export default App
