30a26c673cf21ca86dc1d5865515b739849619e2
uzrafi <uzairrafi159@gmail.com>
2024-10-06 00:47:31 -0400

    Added sign in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 import "./App.css";
 import MapSearch from "./Pages/MapSearchPage";
+import LoginPage from "./Pages/LoginPage";
 function App() {
-  return <MapSearch />;
+  return <LoginPage />;
 }
 
 export default App;
