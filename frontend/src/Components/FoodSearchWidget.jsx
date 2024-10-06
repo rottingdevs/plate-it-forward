5f9907f4bdedc5804065442ec7df0193dd86df4c
uzrafi <uzairrafi159@gmail.com>
2024-10-06 05:05:04 -0400

    Added search for food items

--- frontend/src/Components/FoodSearchWidget.jsx
+++ frontend/src/Components/FoodSearchWidget.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/jsx-key */
-// import { useEffect } from "react";
+import React, { useState } from 'react';
 import "../App.css";
 import FoodCard from "./FoodCard";
 
@@ -21,26 +20,20 @@ function FoodSearchWidget() {
     },
     {
       name: "Strawberries",
-      description: "Quarter pack of starwberries.",
+      description: "Quarter pack of strawberries.",
       expiry: 4,
       image: "./strawberries.png",
       user: "Bobby Brown",
     },
   ];
 
-  // function filterResults(ItemsArr, SearchStr) {
-  //   var input = document.getElementById('searchInput');
-  //   var filter = input.value.toUpperCase();
+  // State to hold the search query
+  const [searchQuery, setSearchQuery] = useState("");
 
-  //   for (i = 0; i < li.length; i++) {
-  //     a = li[i].getElementsByTagName("FoodCard");
-  //     txtValue = a.textContent || a.innerText;
-  //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-  //       li[i].style.display = "";
-  //     } else {
-  //       li[i].style.display = "none";
-  //     }
-  // }
+  // Function to filter food items based on search query
+  const filteredFoodItems = foodItems.filter((food) =>
+    food.name.toLowerCase().includes(searchQuery.toLowerCase()) // Case-insensitive search
+  );
 
   return (
     <div className="foodSearchDiv">
@@ -52,6 +45,8 @@ function FoodSearchWidget() {
           placeholder="Search nearby goods"
           name="search"
           id="searchInput"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button
           style={{
@@ -63,13 +58,17 @@ function FoodSearchWidget() {
             top: 187,
           }}
         >
-          <img src="./search_icon.svg" />
+          <img src="./search_icon.svg" alt="Search" />
         </button>
-        {/* This is where food cards are*/}
+        {/* Display filtered food cards */}
         <div className="scrollable">
-          {foodItems.map((food) => (
-            <FoodCard foodItem={food}></FoodCard>
-          ))}
+          {filteredFoodItems.length > 0 ? (
+            filteredFoodItems.map((food, index) => (
+              <FoodCard key={index} foodItem={food}></FoodCard>
+            ))
+          ) : (
+            <p>No food items found.</p>
+          )}
         </div>
         <button className="button">Select item(s)</button>
       </div>
