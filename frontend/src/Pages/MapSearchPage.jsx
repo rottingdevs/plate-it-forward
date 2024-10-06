1eaa80ed2cb7cfac95217d85207d24921d9b34e6
Uzair Rafi <112304028+uzrafi@users.noreply.github.com>
2024-10-06 07:39:30 -0400

    Feature/food search and pickup (#12)
    * Added search for food items
    
    * Fixed search, added listing component
    
    * Added done button functionality

--- frontend/src/Pages/MapSearchPage.jsx
+++ frontend/src/Pages/MapSearchPage.jsx
@@ -1,17 +1,31 @@
+import React, { useState } from 'react';
 import NavBar from "../Components/NavBar";
 import Map from "@/Components/Map";
 import ConfirmFoodWidget from "../Components/ConfirmFoodWidget";
-
-import "../App.css";
 import FoodSearchWidget from "../Components/FoodSearchWidget";
+import "../App.css";
 
 function MapSearchPage() {
-  const state = true;
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [isWidgetVisible, setWidgetVisible] = useState(true); // State to control widget visibility
+
+  const handleSelectItems = (items) => {
+    setSelectedItems(items);
+    setWidgetVisible(true);
+  };
+
+  const handleCloseWidget = () => {
+    setWidgetVisible(false);
+  };
+
   return (
     <>
       <NavBar />
-      <FoodSearchWidget />
-      {state ? <ConfirmFoodWidget /> : <></>}
+      <FoodSearchWidget onSelectItems={handleSelectItems} />
+      {/* Conditionally render ConfirmFoodWidget if the widget is visible */}
+      {isWidgetVisible && (
+        <ConfirmFoodWidget selectedItems={selectedItems} onClose={handleCloseWidget} />
+      )}
       <Map />
     </>
   );
