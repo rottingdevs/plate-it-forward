ac25059038c6dda5b1b5f06ec07543286b3308b6
uzrafi <uzairrafi159@gmail.com>
2024-10-06 01:55:26 -0400

    Added routers, added signup label

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,19 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
 import MapSearch from "./Pages/MapSearchPage";
 import LoginPage from "./Pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <LoginPage />;
+  return (
+    // add routers
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MapSearch />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/search" element={<MapSearch />} />
+      </Routes>
+    </BrowserRouter>
+  ) 
 }
 
 export default App;
