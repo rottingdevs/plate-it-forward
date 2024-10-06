bb6dc2bb003ede5f9a0db829ad5dcc9eaadac55e
uzrafi <uzairrafi159@gmail.com>
2024-10-06 04:46:11 -0400

    finish sign up flows

--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -1,43 +1,54 @@
 import React, { useState } from 'react';
 import NavBar from '../Components/NavBar';
 import '../App.css';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate } from 'react-router-dom';
 
 function SignUpPage() {
-    const [email, setEmail] = useState('');
-    return (
-        <>
-        <NavBar />
-          <div className="login-container">
-            <div className="login-box">
-              <h1>Sign up for Plate It Forward</h1>
-              <form>
-                <div className="input-group">
-                  <label>Email address</label>
-                  <input 
-                    type="email"
-                    placeholder="Enter email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-                </div>
-                <div className="input-group">
-                  <label>Create Password</label>
-                  <input
-                    type="password"
-                    placeholder="Enter password"
-                  />
-                </div>
-                <button className="login-button" type="submit">Sign up</button>
-                <div className="newaccount-group">
-                <label>Already have an account? <Link to="/login">Sign in</Link>
-                </label>
-                </div>     
-              </form>
-            </div>
-          </div>
-        </>
-      );      
-    }
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate(); 
 
-    export default SignUpPage;
+       const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent page refresh on form submission
+        navigate('/');
+    };
+
+
+  return (
+    <>
+      <NavBar />
+      <div className="login-container">
+        <div className="login-box">
+          <h1>Sign up for Plate It Forward</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label>Username</label>
+              <input
+                type="username"
+                placeholder="Enter username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="input-group">
+              <label>Create Password</label>
+              <input
+                type="password"
+                placeholder="Enter password"
+              />
+            </div>
+            <button className="login-button" type="submit">
+              Sign up
+            </button>
+            <div className="newaccount-group">
+              <label>
+                Already have an account? <Link to="/login">Sign in</Link>
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUpPage;
