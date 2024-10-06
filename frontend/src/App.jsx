b5e6779947e3f536ac2d9f68422378f1a09ce899
Uzair Rafi <112304028+uzrafi@users.noreply.github.com>
2024-10-06 02:14:57 -0400

    Feature/signup page (#7)
    * Added sign in page
    
    * Added routers, added signup label
    
    * Added routes and made sign up page
    
    * remove unused h3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,22 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 import "./App.css";
 import MapSearch from "./Pages/MapSearchPage";
+import LoginPage from "./Pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUpPage from "./Pages/SignUpPage";
+
 function App() {
-  return <MapSearch />;
+  return (
+    // add routers
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MapSearch />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/search" element={<MapSearch />} />
+      </Routes>
+    </BrowserRouter>
+  ) 
 }
 
 export default App;
